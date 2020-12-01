838468331213f3b918fb2a82ec514f14c6a6d721
Nerijus <nerijus.bucinskis@gmail.com>
2020-12-01 18:59:27 +0200

    Day 1 solved

--- Day01_2.java
+++ Day01_2.java
@@ -1,23 +1,25 @@
 import java.util.List;
 
 /**
- * Part one of
+ * Part two of
  * https://adventofcode.com/2020/day/1
  *
  * @author Nerijus
  */
-public class Day01_1 {
+public class Day01_2 {
     public static void main(String[] args) {
-        System.out.println("Multiplied: " + new Day01_1().getResult());
+        System.out.println("Multiplied: " + new Day01_2().getResult());
     }
 
     private int getResult() {
         List<Integer> entries = Inputs.readInts("Day01");
         for (Integer first : entries) {
             for (Integer second : entries) {
-                if (first + second == 2020) {
-                    System.out.printf("%d + %d = 2020\n", first, second);
-                    return first * second;
+                for (Integer third : entries) {
+                    if (first + second + third == 2020) {
+                        System.out.printf("%d + %d + %d = 2020\n", first, second, third);
+                        return first * second * third;
+                    }
                 }
             }
         }
